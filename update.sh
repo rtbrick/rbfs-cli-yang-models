d5c660ed5c1a780c5c770211165c8715e46dbdcd
Chris Lenz <chris@rtbrick.com>
2024-07-04 15:59:59 +0000

    Add update script.sh

---
+++ update.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+VERSION=${1}
+BRANCH=${2}
+TAG=${3}
+
+function prepare() {
+  git checkout main
+  git pull
+  git checkout -b "$BRANCH" || true
+  git checkout "$BRANCH"
+  rm -rf accessleaf
+  rm -rf consolidated-bng
+  rm -rf l2bsa
+  rm -rf spine
+}
+function commit() {
+  git add .
+  git commit -m "Add yang files for $VERSION"
+  git push --set-upstream origin "$BRANCH"
+}
+function loadData() {
+   # Use jq to iterate over each item in the array directly
+   rtb-image list -v "$VERSION" -f lxd -O json \
+     | jq -c '.[] | select(.Platform != "all" and .Platform != "virtual")' \
+     | while IFS= read -r item; do
+     uuid=$(echo "$item" | jq -r '.UUID')
+     platform=$(echo "$item" | jq -r '.Platform')
+     model=$(echo "$item" | jq -r '.Model')
+     role=$(echo "$item" | jq -r '.Role')
+     version=$(echo "$item" | jq -r '.Version')
+
+     todir="$role/$model"
+     mountdir="temp/mount"
+     # Use the extracted 'UUID' and 'Platform' in your script
+     if [[ "$model" == "combined" ]]; then
+       todir="$role/$model/$platform"
+     fi
+
+     # Normal processing
+     echo "Processing $uuid $role $model $platform $version"
+     echo "* pull $uuid"
+     sudo rtb-image pull "$uuid"
+
+     mkdir -p "temp"
+     echo "* mount $mountdir"
+     sudo rtb-image mount -d "$mountdir" "$uuid"
+
+     echo "* to dir $todir"
+     mkdir -p "$todir"
+     echo "copy from $mountdir/usr/share/rtbrick/rtbcli/yang to $todir"
+
+     sudo cp -r "$mountdir/usr/share/rtbrick/rtbcli/yang" "$todir"
+     echo "done"
+
+     echo "* umount $mountdir"
+     sudo umount "$mountdir"
+     sudo rm -rf "temp"
+   done
+}
+
+prepare
+loadData
+commit
